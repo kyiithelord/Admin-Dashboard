5f2f3335a48638ca3e1cd81df9228318e1af1e80
kyiithelord <thetzinkyaw9727@gmail.com>
2024-11-28 11:16:21 +0630

    Upcoming Events Added

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import { ProjectCardSkeleton } from "./skeleton/project-card";
+import { KanbanColumnSkeleton } from "./skeleton/kanban";
+import { AccordionHeaderSkeleton } from "./skeleton/according-header";
+export {UpcomingEvents,DealsChart,UpcomingEventsSkeleton,ProjectCardSkeleton,KanbanColumnSkeleton,AccordionHeaderSkeleton};
