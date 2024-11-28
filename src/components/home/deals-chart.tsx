92f92547b58caac3344f3b50efca3a3a98ac7deb
kyiithelord <thetzinkyaw9727@gmail.com>
2024-11-28 14:40:03 +0630

    Deals Chart Added

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,6 +1,85 @@
+import { DollarOutlined } from "@ant-design/icons"
+import { Card } from "antd"
+import { Text } from "../text"
+import { Area, AreaConfig } from "@ant-design/plots"
+import { useList } from "@refinedev/core"
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries"
+import React from "react"
+import { mapDealsData } from "@/utilities/helpers"
+import { DashboardDealsChartQuery } from "@/graphql/type"
+import { GetFieldsFromList } from "@refinedev/nestjs-query"
+
 const DealsChart = () => {
+  const {data} = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource:'dealStages',
+    filters:[
+      {field:'title',operator:'in',value:['WON','LOST']}
+    ],
+    meta:{
+      gqlQuery:DASHBOARD_DEALS_CHART_QUERY
+    }
+  });
+
+  const dealData = React.useMemo(()=>{
+    return mapDealsData(data?.data); 
+  },[data?.data])
+  const config: AreaConfig = {
+    data: dealData,
+    xField: 'timeText',
+    yField: 'value',
+    isStack: false,
+    seriesField: 'state',
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    // yAxis: {animation: true,
+    //   tickCount: 4,
+    //   // label: {
+    //   //   formatter: (v: string) => {
+    //   //     return `$${Number(v) / 1000}k`; // Formats the Y-axis labels
+    //   //   },
+    //   // },
+    // },
+    // tooltip: {
+    //   formatter: (data) => {
+    //     return {
+    //       name: data.state, // State or category name
+    //       value: `$${Number(data.value) / 1000}k`, // Correctly display value as $Xk
+    //     };
+    //   },
+    // },
+  };
+  
   return (
-    <div>DealsChart</div>
+    <Card
+      style={{height:'100%'}} // Root style
+      styles={{
+        header: { padding:'8px 16px' },
+        body: { padding: '24px 24px 0 24px'},
+      }}
+      title={
+        <div
+          style={{
+            display:'flex',
+            alignItems:'center',
+            gap:'8px'
+          }}
+        >
+           <DollarOutlined/>
+            <Text
+              size="sm" 
+              style={{marginLeft:'0.5rem'}}
+            >
+              Deals
+            </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325}/>
+    </Card>
   )
 }
 
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -38,5 +38,119 @@ export const DASHBOARD_CALENDAR_UPCOMING_EVENTS_QUERY = gql `
 `;
 
 
-// Query to get deals chart
+
+export const DASHBOARD_DEALS_CHART_QUERY = gql`
+  query DashboardDealsChart(
+    $filter: DealStageFilter!
+    $sorting: [DealStageSort!]
+    $paging: OffsetPaging
+  ) {
+    dealStages(filter: $filter, sorting: $sorting, paging: $paging) {
+      nodes {
+        id
+        title
+        dealsAggregate {
+          groupBy {
+            closeDateMonth
+            closeDateYear
+          }
+          sum {
+            value
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`;
+
+
+export const DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY = gql`
+  query DashboardLatestActivitiesDeals(
+    $filter: DealFilter!
+    $sorting: [DealSort!]
+    $paging: OffsetPaging
+  ) {
+    deals(filter: $filter, sorting: $sorting, paging: $paging) {
+      totalCount
+      nodes {
+        id
+        title
+        stage {
+          id
+          title
+        }
+        company {
+          id
+          name
+          avatarUrl
+        }
+        createdAt
+      }
+    }
+  }
+`;
+
+export const DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY = gql`
+  query DashboardLatestActivitiesAudits(
+    $filter: AuditFilter!
+    $sorting: [AuditSort!]
+    $paging: OffsetPaging
+  ) {
+    audits(filter: $filter, sorting: $sorting, paging: $paging) {
+      totalCount
+      nodes {
+        id
+        action
+        targetEntity
+        targetId
+        changes {
+          field
+          from
+          to
+        }
+        createdAt
+        user {
+          id
+          name
+          avatarUrl
+        }
+      }
+    }
+  }
+`;
+
+
+export const COMPANIES_LIST_QUERY = gql`
+  query CompaniesList(
+    $filter: CompanyFilter!
+    $sorting: [CompanySort!]
+    $paging: OffsetPaging!
+  ) {
+    companies(filter: $filter, sorting: $sorting, paging: $paging) {
+      totalCount
+      nodes {
+        id
+        name
+        avatarUrl
+        dealsAggregate {
+          sum {
+            value
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const CREATE_COMPANY_MUTATION = gql`
+  mutation CreateCompany($input: CreateOneCompanyInput!) {
+    createOneCompany(input: $input) {
+      id
+      salesOwner {
+        id
+      }
+    }
+  }
+`;
 
